8a3f2054768325d7b33ee89b13999e34fb0d1f23
Rely Kao <rely_kao@asiavista.com.tw>
2022-01-13 17:47:29 +0800

    commit

---
+++ CoravelTaskWorkerLab/Jobs/MyFirstCoravelJob.cs
@@ -0,0 +1,25 @@
+﻿using Coravel.Invocable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoravelTaskWorkerLab.Jobs;
+
+internal class MyFirstCoravelJob : IInvocable
+{
+    readonly ILogger<MyFirstCoravelJob> _logger;
+
+    public MyFirstCoravelJob(ILogger<MyFirstCoravelJob> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Invoke()
+    {
+        _logger.LogInformation("這是我的第一支 Coravel Job。");
+
+        return Task.CompletedTask;
+    }
+}
--- CoravelTaskWorkerLab/Program.cs
+++ CoravelTaskWorkerLab/Program.cs
@@ -1,5 +1,7 @@
-using Serilog;
+﻿using Serilog;
 using CoravelTaskWorkerLab;
+using Coravel;
+using CoravelTaskWorkerLab.Jobs;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureLogging((ctx, logging) =>
@@ -12,9 +14,22 @@ IHost host = Host.CreateDefaultBuilder(args)
     })
     .ConfigureServices((ctx, services) =>
     {
-        services.AddHostedService<Worker>();
+        services.AddScheduler();
+        services.AddTransient<MyFirstCoravelJob>(); // 需預先註冊Job這不太實用。
+
+        //## 註冊 Worker Service 
+        //services.AddHostedService<Worker>();
     })
     .UseWindowsService()
     .Build();
+
+host.Services.UseScheduler(scheduler =>
+{
+    //## 在此設定工作排程。但不易動態設定故不太實用。
+    scheduler
+        .Schedule<MyFirstCoravelJob>()
+        .EveryFiveSeconds()
+        .Weekday();
+});
 
 await host.RunAsync();
